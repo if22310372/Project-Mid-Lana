f3270ad04361b1f0291a03849fbee6dbb5afc880
Lana Londah <S22310372@student.unklab.ac.id>
2024-11-17 23:58:52 +0800

    change bit css

---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1PQq7QBbHt0Z_tV5rDOHMmK8Trkzl60Q",
+  authDomain: "frontend-lana.firebaseapp.com",
+  databaseURL: "https://frontend-lana-default-rtdb.firebaseio.com",
+  projectId: "frontend-lana",
+  storageBucket: "frontend-lana.firebasestorage.app",
+  messagingSenderId: "6633671793",
+  appId: "1:6633671793:web:9fde54bb0641de71599fd0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
